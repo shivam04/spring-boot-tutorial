c52b742c241e2c77e1320217c3628f53d9778f09
Shivam Sinha <shivam.si@media.net>
2021-02-24 08:58:46 +0000

    1st lesson hello world

---
+++ spring-boot-starter-web/src/test/java/com/example/springbootstarterweb/SpringBootStarterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstarterweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
