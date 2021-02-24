687193d2102320367ac46781b1f03db8c268087f
Shivam Sinha <shivam.si@media.net>
2021-02-24 14:09:29 +0000

    15th lesson cron

---
+++ springboot-scheduling/src/main/java/com/example/springbootscheduling/scheduler/Scheduler.java
@@ -0,0 +1,38 @@
+package com.example.springbootscheduling.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by shivam.si on 24/02/21 2:05 pm
+ */
+@Component
+public class Scheduler {
+    @Scheduled(cron = "* * * * * ?")
+    public void cronJobSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job expression:: " + strDate);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void fixedRateSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Fixed Rate scheduler:: " + strDate);
+    }
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 3000)
+    public void fixedDelaySch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Fixed Delay scheduler:: " + strDate);
+    }
+}
