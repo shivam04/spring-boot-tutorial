bcb1bd06d34369312e2cf97d16e4861b1d564b6b
Shivam Sinha <shivam.si@media.net>
2021-02-24 15:01:39 +0000

    16th lesson eureka

---
+++ springboot-service-registration-with-eureka/src/test/java/com/example/springbootserviceregistrationwitheureka/SpringbootServiceRegistrationWithEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootserviceregistrationwitheureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceRegistrationWithEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
