b428579972fdbd294a478a482134a0f3d31c3cca
Shivam Sinha <shivam.si@media.net>
2021-02-24 12:51:36 +0000

    10th lesson file upload

---
+++ springboot-file-upload/src/main/java/com/example/springbootfileupload/controller/FileUploadController.java
@@ -0,0 +1,30 @@
+package com.example.springbootfileupload.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by shivam.si on 24/02/21 12:41 pm
+ */
+@RestController
+public class FileUploadController {
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+}
