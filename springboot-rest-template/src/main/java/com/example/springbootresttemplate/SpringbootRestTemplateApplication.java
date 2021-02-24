b3f7cec73eea6f06e79c60becd7f6284a6875226
Shivam Sinha <shivam.si@media.net>
2021-02-24 12:37:51 +0000

    9th lesson rest template

---
+++ springboot-rest-template/src/main/java/com/example/springbootresttemplate/SpringbootRestTemplateApplication.java
@@ -0,0 +1,19 @@
+package com.example.springbootresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringbootRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestTemplateApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ springboot-rest-template/src/main/java/com/example/springbootresttemplate/model/Product.java
@@ -0,0 +1,25 @@
+package com.example.springbootresttemplate.model;
+
+/**
+ * Created by shivam.si on 24/02/21 12:15 pm
+ */
+public class Product {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
