9393bc3ae0b1082cd44895e952f7629c5de3c234
Shivam Sinha <shivam.si@media.net>
2021-02-24 09:39:21 +0000

    2nd lesson tomcat deployment

---
+++ spring-boot-tomcat-deployment/src/main/java/com/example/springboottomcatdeployment/SpringBootTomcatDeploymentApplication.java
@@ -0,0 +1,28 @@
+package com.example.springboottomcatdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootTomcatDeploymentApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootTomcatDeploymentApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatDeploymentApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World from Tomcat";
+    }
+
+}
