ac9debae8f6df09c8f3b5628312370b519a3e21d
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-09 17:50:27 +0000

    flyway db

---
+++ springboot-flyway-db/src/test/java/com/example/springbootflywaydb/SpringbootFlywayDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootflywaydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFlywayDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
