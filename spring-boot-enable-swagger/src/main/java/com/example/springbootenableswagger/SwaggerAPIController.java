2619ef00897bf71916779458c85dd1e3d53493b1
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-09 15:06:41 +0000

    swagger

---
+++ spring-boot-enable-swagger/src/main/java/com/example/springbootenableswagger/SwaggerAPIController.java
@@ -0,0 +1,27 @@
+package com.example.springbootenableswagger;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shivam.si on 09/02/22 2:58 pm
+ */
+
+@RestController
+public class SwaggerAPIController {
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<String> getProducts() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public String createProduct() {
+        return "Product is saved successfully";
+    }
+}
---
+++ spring-boot-enable-swagger/src/test/java/com/example/springbootenableswagger/SpringBootEnableSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootenableswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnableSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
