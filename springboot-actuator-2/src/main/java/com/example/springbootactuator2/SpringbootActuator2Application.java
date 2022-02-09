d363d24a5c2881180196aad2f702c347c8ff7799
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-09 14:25:03 +0000

    spring boot admin client server

---
+++ springboot-actuator-2/src/main/java/com/example/springbootactuator2/SpringbootActuator2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootactuator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuator2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActuator2Application.class, args);
+    }
+
+}
