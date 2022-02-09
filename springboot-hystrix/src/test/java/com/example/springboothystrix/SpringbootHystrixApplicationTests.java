6cb4dbd9e338ed6ccf89ba466bb4012f0004c4c8
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-09 18:45:45 +0000

    hystrix

---
+++ springboot-hystrix/src/test/java/com/example/springboothystrix/SpringbootHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
