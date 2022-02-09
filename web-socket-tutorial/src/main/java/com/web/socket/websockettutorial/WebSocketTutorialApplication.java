18af3bd913997190619b46a78fd820ab4ae9bb0c
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-09 13:08:15 +0000

    chat socket

---
+++ web-socket-tutorial/src/main/java/com/web/socket/websockettutorial/WebSocketTutorialApplication.java
@@ -0,0 +1,11 @@
+package com.web.socket.websockettutorial;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class WebSocketTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketTutorialApplication.class, args);
+    }
+
+}
---
+++ web-socket-tutorial/src/test/java/com/web/socket/websockettutorial/WebSocketTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.socket.websockettutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
