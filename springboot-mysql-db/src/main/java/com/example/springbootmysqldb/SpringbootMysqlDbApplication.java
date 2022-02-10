db20649d7697d9495a1102870615177066701777
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-10 12:14:06 +0000

    spring boot mysql

---
+++ springboot-mysql-db/src/main/java/com/example/springbootmysqldb/SpringbootMysqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMysqlDbApplication.class, args);
+    }
+
+}
---
+++ springboot-mysql-db/src/test/java/com/example/springbootmysqldb/SpringbootMysqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
