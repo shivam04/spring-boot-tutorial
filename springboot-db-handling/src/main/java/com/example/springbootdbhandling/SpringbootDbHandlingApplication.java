d7fc7c0ef41175dfc54858be2c44cfd531eae227
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-10 11:50:15 +0000

    spring boot h2

---
+++ springboot-db-handling/src/main/java/com/example/springbootdbhandling/SpringbootDbHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdbhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDbHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDbHandlingApplication.class, args);
+    }
+
+}
---
+++ springboot-db-handling/src/test/java/com/example/springbootdbhandling/SpringbootDbHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdbhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
