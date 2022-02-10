41d4d5d64b4553ede87675129738b268df95c125
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2022-02-10 13:10:07 +0000

    spring boot web security

---
+++ springboot-web-app/src/main/java/com/example/springbootwebapp/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.springbootwebapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by shivam.si on 10/02/22 12:50 pm
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
